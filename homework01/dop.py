4a0599e394d47709e34e51ec83d810ec807cda43
Ruslan Berngardt <lgun44173@gmail.com>
2025-11-05 14:43:12 +0300

    Реализована функция с шифрованием с последовательным сдвигом

---
+++ homework01/dop.py
@@ -0,0 +1,21 @@
+'''Has a module that encrypts the text'''
+def encrypt_growing_shift(plaintext, start, delta):
+    '''Encrypts the text'''
+    alphabet_lower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    alphabet_upper = alphabet_lower.upper()
+    result = []
+    shift = start
+    for char in plaintext:
+        if char in alphabet_lower:
+            idx = alphabet_lower.index(char)
+            new_idx = (idx + shift) % len(alphabet_lower)
+            result.append(alphabet_lower[new_idx])
+            shift += delta
+        elif char in alphabet_upper:
+            idx = alphabet_upper.index(char)
+            new_idx = (idx + shift) % len(alphabet_upper)
+            result.append(alphabet_upper[new_idx])
+            shift += delta
+        else:
+            result.append(char)
+    return ''.join(result)
